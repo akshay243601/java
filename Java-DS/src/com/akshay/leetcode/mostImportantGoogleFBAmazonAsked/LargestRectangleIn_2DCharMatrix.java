30ca5753dd4bf7fbd3aa4e84534152855078d99a
Akshay Gupta <akshay3.gupta@paytm.com>
2021-03-29 01:16:21 +0530

    Java Code

--- Java-DS/src/com/akshay/leetcode/mostImportantGoogleFBAmazonAsked/LargestRectangleIn_2DCharMatrix.java
+++ Java-DS/src/com/akshay/leetcode/mostImportantGoogleFBAmazonAsked/LargestRectangleIn_2DCharMatrix.java
@@ -1,9 +1,41 @@
 package com.akshay.leetcode.mostImportantGoogleFBAmazonAsked;
 
 import java.util.Stack;
-
+/******
+ *
+ * GOOGLE/AMAZON/FB Important
+ * https://leetcode.com/problems/maximal-rectangle/
+ * Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+ *
+ * Example 1:
+ * Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * Output: 6
+ * Explanation: The maximal rectangle is shown in the above picture.
+ *
+ * Example 2:
+ * Input: matrix = []
+ * Output: 0
+ *
+ * Example 3:
+ * Input: matrix = [["0"]]
+ * Output: 0
+ *
+ * Example 4:
+ * Input: matrix = [["1"]]
+ * Output: 1
+ *
+ * Example 5:
+ * Input: matrix = [["0","0"]]
+ * Output: 0
+ *
+ * **********/
 public class LargestRectangleIn_2DCharMatrix {
 
+    /**
+     * Best Explanation
+     * https://www.youtube.com/watch?v=g8bSdXCG-lA
+     * https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/MaximumRectangularSubmatrixOf1s.java
+     * ****/
     public int maximalRectangle(char[][] matrix) {
         if(matrix == null || matrix.length == 0) {
             return 0;
@@ -14,7 +46,7 @@ public class LargestRectangleIn_2DCharMatrix {
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
                 if(matrix[i][j] != '0') {
-                    temp[j] += 1;
+                    temp[j] = temp[j] + 1;
                 } else {
                     temp[j] = 0;
                 }
